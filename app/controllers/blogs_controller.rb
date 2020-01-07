4bfb669fcddc69cab60fbdb2c5443b5f9563bb1b
古家歌織 <furuyakaori@kaori-f.local>
2020-01-07 13:38:04 +0900

    [Finish4-3]

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,10 +4,22 @@ class BlogsController < ApplicationController
 
   def show
   end
-
+# 投稿フォーム表示
   def new
+  	@blog = Blog.new
+  end
+#保存
+  def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
   end
 
   def edit
+  end
+
+private
+  def blog_params
+  	params.require(:blog).permit(:title, :category, :body)
   end
 end
